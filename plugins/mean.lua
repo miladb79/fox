1ff4709469d902d58dfdf8e411185a53813e4f2d
miladb79 <miladbageri137979@gmail.com>
2016-06-11 03:24:49 +0430

    Create mean.lua

---
+++ plugins/mean.lua
@@ -0,0 +1,23 @@
+local function run(msg, matches)
+  local htp = http.request('http://api.vajehyab.com/v2/public/?q='..URL.escape(matches[1]))
+  local data = json:decode(htp)
+	return 'کلمه : '..(data.data.title or data.search.q)..'\n\nمعنی : '..(data.data.text or '----' )..'\n\nمنبع : '..(data.data.source or '----' )..'\n\n'..(data.error.message or '')..'\n\nby milad'
+end
+return {
+  patterns = {
+    "^[!#/][Mm][Ee][Aa][Nn] (.*)$"
+  },
+  run = run
+}
+
+--[[
+
+نویسنده : سید محمد خشنوا
+@cruel 
+کانال ما
+
+cruel_channel
+
+
+
+]]
